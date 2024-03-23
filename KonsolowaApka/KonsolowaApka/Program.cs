76381948fd6fd940f35d77d6e83fb2b59faacdaf
s24230 <s24230@pjwstk.edu.pl>
2024-03-23 18:42:00 +0100

    Modyfikacja 2

--- KonsolowaApka/KonsolowaApka/Program.cs
+++ KonsolowaApka/KonsolowaApka/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello honey!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello precious");
         }
     }
 }
